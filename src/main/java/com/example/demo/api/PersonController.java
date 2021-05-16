2a4e7f71cbb40900d6f048e17138528691ef8b18
dwyfrequency <jdwy215@gmail.com>
2021-05-16 15:21:48 -0400

    add crud operations in api

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,7 +2,13 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
 
     private final PersonService personService;
@@ -11,7 +17,28 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public int deletePersonById(@PathVariable("id") UUID id) {
+        return personService.deletePersonById(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public int updatePerson(@PathVariable("id") UUID id, @RequestBody Person updatedPerson) {
+        return personService.updatePersonById(id, updatedPerson);
     }
 }
--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-@Repository
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
     private static List<Person> DB = new ArrayList<>();
 
@@ -17,4 +19,40 @@ public class FakePersonDataAccessService implements PersonDao {
         return 1;
     }
 
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        DB = DB.stream()
+                .filter(person -> !person.getId().equals(id))
+                .collect(Collectors.toList());
+
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return selectPersonById(id)
+                .map(personToReplace -> {
+                    int indexOfPersonToReplace = DB.indexOf(personToReplace);
+                    if (indexOfPersonToReplace >= 0) {
+                        DB.set(indexOfPersonToReplace, person);
+                        return 1;
+                    }
+                    return 0;
+                }).orElse(0);
+    }
 }
